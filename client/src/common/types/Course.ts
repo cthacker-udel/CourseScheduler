e7e0839265f71c14642ad7970afee6a7bcce3ea7
cthacker-udel <cthacker@udel.edu>
2022-05-22 16:32:14 -0400

    [main] Restored file status

--- client/src/common/types/Course.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Course type, only contains the name, and title, the credits, and pre-reqs will be handled by the loading of the json
- */
-type Course = {
-    name: string;
-    title?: string;
-};
-
-export default Course;
