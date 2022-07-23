cb895082633ea2cb939611b9b4205202b18914bb
cthacker@udel.edu <cthacker@udel.edu>
2022-07-23 14:50:46 -0400

    [next-config] Removed unused dependency

--- server/src/modules/User/user.module.ts
+++ server/src/modules/User/user.module.ts
@@ -3,7 +3,6 @@ import { CryptoModule } from "../Crypto/crypto.module";
 import { UserService } from "./user.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/entities";
-import { CryptoService } from "../Crypto/crypto.service";
 
 /**
  * The user module, exports the UserService while importing the SchemaModule
