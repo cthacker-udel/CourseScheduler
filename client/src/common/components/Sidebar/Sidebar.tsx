e5cda3da0f89385e250c338ea517461d33bbe1f6
cthacker-udel <cthacker@udel.edu>
2022-07-28 04:10:22 -0400

    [sidebar] Added individual content hover effect, and displaying all options

--- client/src/common/components/Sidebar/Sidebar.tsx
+++ client/src/common/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,25 @@
-import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import {
+    faBook,
+    faBookAtlas,
+    faCaretLeft,
+    faCaretRight,
+    faClinicMedical,
+    faClipboard,
+    faDownload,
+    faFileExport,
+    faHome,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { OverlayTrigger } from "react-bootstrap";
 import { generateTooltip } from "src/helpers";
 
 import styles from "./Sidebar.module.css";
+
+const SIDEBAR_CONSTANTS = {
+    HEADER: "Options",
+};
+
 /**
  * This is the Sidebar component, which will allow the user to navigate throughout the application with ease
  *
@@ -49,8 +64,63 @@ export const Sidebar = (): JSX.Element => {
                         : styles.sidebar_content_open
                 }
             >
-                <span>{"Hello"}</span>
-                <span>{"there"}</span>
+                <span className="text-center py-2 fw-bold text-decoration-underline border-2 border-secondary border-bottom">
+                    {SIDEBAR_CONSTANTS.HEADER}
+                </span>
+                <span
+                    className={`${styles.sidebar_individual_content} d-flex flex-row justify-content-center pt-2 border-bottom pb-2`}
+                    role="button"
+                >
+                    <FontAwesomeIcon className="my-auto pe-1" icon={faHome} />
+                    <span className="fw-bold">{"Home"}</span>
+                </span>
+                <span
+                    className={`${styles.sidebar_individual_content} d-flex flex-row justify-content-center pt-2 border-bottom pb-2`}
+                    role="button"
+                >
+                    <FontAwesomeIcon className="my-auto pe-1" icon={faBook} />
+                    <span className="fw-bold">{"Courses"}</span>
+                </span>
+                <span
+                    className={`${styles.sidebar_individual_content} d-flex flex-row justify-content-center pt-2 border-bottom pb-2`}
+                    role="button"
+                >
+                    <FontAwesomeIcon
+                        className="my-auto pe-1"
+                        icon={faBookAtlas}
+                    />
+                    <span className="fw-bold">{"Semesters"}</span>
+                </span>
+                <span
+                    className={`${styles.sidebar_individual_content} d-flex flex-row justify-content-center pt-2 border-bottom pb-2`}
+                    role="button"
+                >
+                    <FontAwesomeIcon
+                        className="my-auto pe-1"
+                        icon={faClipboard}
+                    />
+                    <span className="fw-bold">{"Plans"}</span>
+                </span>
+                <span
+                    className={`${styles.sidebar_individual_content} d-flex flex-row justify-content-center pt-2 border-bottom pb-2`}
+                    role="button"
+                >
+                    <FontAwesomeIcon
+                        className="my-auto pe-1"
+                        icon={faDownload}
+                    />
+                    <span className="fw-bold">{"Downloads"}</span>
+                </span>
+                <span
+                    className={`${styles.sidebar_individual_content} d-flex flex-row justify-content-center pt-2 border-bottom pb-2`}
+                    role="button"
+                >
+                    <FontAwesomeIcon
+                        className="my-auto pe-1"
+                        icon={faFileExport}
+                    />
+                    <span className="fw-bold">{"Exports"}</span>
+                </span>
             </div>
         </>
     );
