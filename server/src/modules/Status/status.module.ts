7580b964b58a595778b95afde004ea484739a636
cthacker-udel <cthacker@udel.edu>
2022-07-31 16:17:59 -0400

    [deployment] Renamed status controller

--- server/src/modules/Status/status.module.ts
+++ server/src/modules/Status/status.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { StatusController } from "./Status.controller";
+import { StatusController } from "./status.controller";
 
 @Module({
     controllers: [StatusController],
