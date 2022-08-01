f4cdaca8751ee6eafee722839bca50ae305a4112
cthacker-udel <cthacker@udel.edu>
2022-08-01 03:33:37 -0400

    [dashboard] Added prefetching

--- client/src/modules/Dashboard/Dashboard.tsx
+++ client/src/modules/Dashboard/Dashboard.tsx
@@ -5,20 +5,42 @@ import {
     faRectangleList,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
 import React from "react";
 import { OverlayTrigger } from "react-bootstrap";
 import type { OverlayInjectedProps } from "react-bootstrap/esm/Overlay";
 import { generateTooltip } from "src/helpers";
+import { Logger } from "src/log/Logger";
 
 import styles from "./Dashboard.module.css";
 
 /**
- * This component displays the dashboard
+ * General dashboard for navigating between user applications
  *
- * @returns The dashboard
+ * @returns The dashboard component
  */
 export const Dashboard = (): JSX.Element => {
-    const [randomState, setRandomState] = React.useState(false);
+    const router = useRouter();
+
+    React.useEffect(() => {
+        /**
+         * Prefetches all the routes
+         */
+        const preFetch = async (): Promise<void> => {
+            await router.prefetch("/courses");
+            await router.prefetch("/semesters");
+            await router.prefetch("/plans");
+            await router.prefetch("course-list");
+        };
+        preFetch()
+            .then((): void => {
+                Logger.log("info", "Fetched all routes for Dashboard.tsx");
+            })
+            .catch((error: unknown): void => {
+                Logger.log("error", error);
+            });
+    }, [router]);
+
     return (
         <div className="d-flex flex-column h-100 justify-content-center align-items-center">
             <div className="fs-3 fw-bolder text-center mb-4 text-wrap">
@@ -37,6 +59,7 @@ export const Dashboard = (): JSX.Element => {
                             <FontAwesomeIcon
                                 className={`p-5 border ${styles.dashboard_icon} rounded`}
                                 icon={faBook}
+                                size="3x"
                             />
                         </OverlayTrigger>
                     </span>
@@ -53,6 +76,7 @@ export const Dashboard = (): JSX.Element => {
                             <FontAwesomeIcon
                                 className={`p-5 border ${styles.dashboard_icon} rounded`}
                                 icon={faBookAtlas}
+                                size="3x"
                             />
                         </OverlayTrigger>
                     </span>
@@ -69,6 +93,7 @@ export const Dashboard = (): JSX.Element => {
                             <FontAwesomeIcon
                                 className={`p-5 border ${styles.dashboard_icon} rounded`}
                                 icon={faClipboard}
+                                size="3x"
                             />
                         </OverlayTrigger>
                     </span>
@@ -85,6 +110,7 @@ export const Dashboard = (): JSX.Element => {
                             <FontAwesomeIcon
                                 className={`p-5 border ${styles.dashboard_icon} rounded`}
                                 icon={faRectangleList}
+                                size="3x"
                             />
                         </OverlayTrigger>
                     </span>
