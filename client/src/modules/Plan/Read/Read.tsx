ad9de8621a61272a720bd4eee1e154e8cc3e29c2
cthacker-udel <cthacker@udel.edu>
2022-09-24 03:41:24 -0400

    [login-cookie] Added plan viewer module

--- client/src/modules/Plan/Read/Read.tsx
+++ client/src/modules/Plan/Read/Read.tsx
@@ -1,73 +1,75 @@
+/* eslint-disable no-magic-numbers -- not needed */
 /* eslint-disable no-unused-vars -- disabled for now */
 /* eslint-disable @typescript-eslint/indent -- prettier - eslint errors */
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import chunk from "lodash.chunk";
-import React, { type ChangeEventHandler } from "react";
-import { Form, OverlayTrigger } from "react-bootstrap";
-import type { OverlayInjectedProps } from "react-bootstrap/esm/Overlay";
-import type {
-    Course,
-    CourseSortingReducerSignature,
-    CourseSortingState,
-} from "src/@types";
-import type { CourseSortingActionType } from "src/@types/CourseSorting/CourseSortingAction";
+import React from "react";
+import type { ServerSidePlan } from "src/@types";
 import { CoursePagination } from "src/common";
-import { initialCourseSortState, titleAbbreviationsToTitles } from "src/data";
-import {
-    generateSortingIcon,
-    generateSortingOrderBy,
-    generateTooltip,
-    renderPreRequisites,
-    truncateText,
-} from "src/helpers";
+import { truncateText } from "src/helpers";
 import { useAllPlans } from "src/hooks/Plans/useAllPlans";
-import { useCourses } from "src/hooks/useCourses";
-import { CourseSortingReducer } from "src/reducer";
 
 import _styles from "./Read.module.css";
-
-/**
- * Constants for the Read component
- */
-const CONSTANTS = {
-    // eslint-disable-next-line no-magic-numbers -- no need
-    COURSE_AMOUNT_SELECTIONS: [5, 10, 30, 60],
-    DEFAULT_PAGE: 0,
-    DEFAULT_PAGE_SIZE: 10,
-    DESCRIPTION_LENGTH: 75,
-    EVEN_DIVISIBLE: 2,
-    ID_INDEX: 1,
-    MOD_EVEN: 0,
-    NAME_INDEX: 1,
-    PAGINATION_INC: 1,
-    PLACEHOLDER_FILL: 0,
-    PLACEHOLDER_FILL_SIZE: 6,
-};
-
-const TEXT_CONSTANTS = {
-    INVALID_BREADTH_REQUIREMENTS: "No Breadth Requirements",
-    INVALID_CREDITS: "No Credits",
-    INVALID_DESCRIPTION: "No Description",
-    INVALID_E_BREADTH: "No Elective Breadth",
-    INVALID_PREREQUISITES: "No Pre-Requisites",
-    NUMBER_OF_COURSES: "# of Courses",
-    TABLE_CELL_CLASS_NAME: "w-100 p-3 border",
-    TABLE_HEADERS: ["Section", "Credits", "Description", "Name"],
-    TABLE_SORTING_ICON_FIELDS: ["section", "credits", "description", "name"],
-};
 
 /**
  * General component for viewing courses
  */
 export const Read = (): JSX.Element => {
     const { plans } = useAllPlans();
+    const [currentPage, setCurrentPage] = React.useState(0);
+    const [pageSize, setPageSize] = React.useState(5);
+    const pages = chunk(plans, pageSize);
 
     console.log(plans);
 
     return (
-        <div>
-            <span>{"Hello"}</span>
+        <div className="h-100 d-flex flex-column">
+            <div className="w-75 mx-auto">
+                <div className="text-center w-25 mx-auto fw-bold pb-2 bg-primary bg-opacity-25 mt-4 rounded fs-4">
+                    {"Plan Table"}
+                </div>
+                <div className="d-flex flex-column">
+                    <div className="d-flex flex-row justify-content-between mt-5 mb-3 bg-dark bg-opacity-25 rounded p-2">
+                        <div
+                            className={`${_styles.table_header} text-center fw-bold fs-5`}
+                        >
+                            {"Name"}
+                        </div>
+                        <div
+                            className={`${_styles.table_header} text-center fw-bold fs-5`}
+                        >
+                            {"Semesters"}
+                        </div>
+                    </div>
+                    {plans?.map((eachPlan: ServerSidePlan) => (
+                        <div
+                            className="d-flex flex-row justify-content-between border rounded"
+                            key={eachPlan.name}
+                        >
+                            <div
+                                className={`${_styles.table_row} text-center p-2 border border-top-0 border-start-0 border-bottom-0`}
+                            >
+                                {truncateText(eachPlan.name)}
+                            </div>
+                            <div
+                                className={`${_styles.table_row} text-center p-2`}
+                            >
+                                {JSON.stringify(eachPlan.semesters)}
+                            </div>
+                        </div>
+                    ))}
+                    <div className="mt-5">
+                        <CoursePagination
+                            currentPage={currentPage}
+                            moveToPage={(pageNumber: number): void => {
+                                setCurrentPage(pageNumber);
+                            }}
+                            pagesCount={pages.length}
+                            paginationSize="lg"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
