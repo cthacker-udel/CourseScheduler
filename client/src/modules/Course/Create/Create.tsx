19cef2622391b28a5c044fd1333759694edd0268
cthacker@udel.edu <cthacker@udel.edu>
2022-10-04 03:08:50 -0400

    [login-cookie] Added in fix for create course and course backend as well

--- client/src/modules/Course/Create/Create.tsx
+++ client/src/modules/Course/Create/Create.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import { useAllCourses, useAllLabs } from "src/hooks";
 
 import styles from "./create.module.css";
-import { TEXT } from "./CreateConstants";
+import { TEXT, VALIDATION_TEXT, VALIDATION_VALUES } from "./CreateConstants";
 
 /**
  * Interface for creating a course
@@ -12,8 +12,9 @@ import { TEXT } from "./CreateConstants";
  * @returns The create course module
  */
 export const Create = (): JSX.Element => {
-    const { register, setValue, watch } = useForm({
+    const { formState, register, setValue, watch } = useForm({
         defaultValues: {
+            classSection: "0",
             credits: 0,
             description: "",
             lab: true,
@@ -24,6 +25,10 @@ export const Create = (): JSX.Element => {
         mode: "all",
         reValidateMode: "onChange",
     });
+
+    const { dirtyFields, errors } = formState;
+
+    const [labWatch] = watch(["lab"]);
 
     const { courses } = useAllCourses();
     const { labs } = useAllLabs();
@@ -38,7 +43,25 @@ export const Create = (): JSX.Element => {
                     >
                         <Form.Group className="p-2" controlId="course-name">
                             <Form.Label>{TEXT.FORM1_LABEL}</Form.Label>
-                            <Form.Control type="text" {...register("name")} />
+                            <Form.Control
+                                type="text"
+                                {...register("name", {
+                                    maxLength: {
+                                        message: VALIDATION_TEXT.name.maxLength,
+                                        value: VALIDATION_VALUES.name.maxLength,
+                                    },
+                                    required: {
+                                        message: VALIDATION_TEXT.name.required,
+                                        value: VALIDATION_VALUES.name.required,
+                                    },
+                                })}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors?.name?.message}
+                            </Form.Control.Feedback>
+                            <Form.Control.Feedback type="valid">
+                                {VALIDATION_TEXT.name.valid}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group
                             className="p-2"
@@ -47,15 +70,61 @@ export const Create = (): JSX.Element => {
                             <Form.Label>{TEXT.FORM2_LABEL}</Form.Label>
                             <Form.Control
                                 type="text"
-                                {...register("description")}
+                                {...register("description", {
+                                    maxLength: {
+                                        message:
+                                            VALIDATION_TEXT.description
+                                                .maxLength,
+                                        value: VALIDATION_VALUES.description
+                                            .maxLength,
+                                    },
+                                    required: {
+                                        message:
+                                            VALIDATION_TEXT.description
+                                                .required,
+                                        value: VALIDATION_VALUES.description
+                                            .required,
+                                    },
+                                })}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                {errors?.description?.message}
+                            </Form.Control.Feedback>
+                            <Form.Control.Feedback type="valid">
+                                {VALIDATION_TEXT.description.valid}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group className="p-2" controlId="course-credits">
                             <Form.Label>{TEXT.FORM3_LABEL}</Form.Label>
                             <Form.Control
+                                isInvalid={
+                                    dirtyFields.credits && !!errors.credits
+                                }
+                                isValid={dirtyFields.credits && !errors.credits}
                                 type="number"
-                                {...register("credits")}
+                                {...register("credits", {
+                                    max: {
+                                        message: VALIDATION_TEXT.credits.max,
+                                        value: VALIDATION_VALUES.credits.max,
+                                    },
+                                    min: {
+                                        message: VALIDATION_TEXT.credits.min,
+                                        value: VALIDATION_VALUES.credits.min,
+                                    },
+                                    required: {
+                                        message:
+                                            VALIDATION_TEXT.credits.required,
+                                        value: VALIDATION_VALUES.credits
+                                            .required,
+                                    },
+                                })}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                {errors?.credits?.message}
+                            </Form.Control.Feedback>
+                            <Form.Control.Feedback type="valid">
+                                {VALIDATION_TEXT.credits.valid}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group className="p-2" controlId="section">
                             <Form.Label>{TEXT.FORM4_LABEL}</Form.Label>
@@ -64,10 +133,53 @@ export const Create = (): JSX.Element => {
                                 {...register("section")}
                             />
                         </Form.Group>
+                        <Form.Group className="p-2" controlId="class-section">
+                            <Form.Label>{TEXT.FORM5_LABEL}</Form.Label>
+                            <Form.Control
+                                isInvalid={
+                                    dirtyFields.classSection &&
+                                    !!errors.classSection
+                                }
+                                isValid={
+                                    dirtyFields.classSection &&
+                                    !errors.classSection
+                                }
+                                type="text"
+                                {...register("classSection", {
+                                    maxLength: {
+                                        message:
+                                            VALIDATION_TEXT.classSection
+                                                .maxLength,
+                                        value: VALIDATION_VALUES.classSection
+                                            .maxLength,
+                                    },
+                                    pattern: {
+                                        message:
+                                            VALIDATION_TEXT.classSection
+                                                .pattern,
+                                        value: VALIDATION_VALUES.classSection
+                                            .pattern,
+                                    },
+                                    required: {
+                                        message:
+                                            VALIDATION_TEXT.classSection
+                                                .required,
+                                        value: VALIDATION_VALUES.classSection
+                                            .required,
+                                    },
+                                })}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors?.classSection?.message}
+                            </Form.Control.Feedback>
+                            <Form.Control.Feedback type="valid">
+                                {VALIDATION_TEXT.classSection.valid}
+                            </Form.Control.Feedback>
+                        </Form.Group>
                         <Form.Group className="p-2" controlId="lab-section">
                             <Form.Check>
                                 <Form.Check.Label>
-                                    {TEXT.FORM5_LABEL}
+                                    {TEXT.FORM6_LABEL}
                                 </Form.Check.Label>
                                 <Form.Check.Input
                                     checked={labWatch}
@@ -80,10 +192,10 @@ export const Create = (): JSX.Element => {
                             </Form.Check>
                         </Form.Group>
                         <Form.Group controlId="lab-course">
-                            <Form.Label>{TEXT.FORM6_LABEL}</Form.Label>
+                            <Form.Label>{TEXT.FORM7_LABEL}</Form.Label>
                         </Form.Group>
                         <Form.Group controlId="course-prereqs">
-                            <Form.Label>{TEXT.FORM7_LABEL}</Form.Label>
+                            <Form.Label>{TEXT.FORM8_LABEL}</Form.Label>
                         </Form.Group>
                     </Form>
                 </Card.Body>
