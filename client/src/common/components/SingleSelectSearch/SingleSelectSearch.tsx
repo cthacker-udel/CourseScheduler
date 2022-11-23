8fd44936105193fc0a2b7085b88f1b5bac354256
cthacker-udel <cthacker@udel.edu>
2022-11-22 22:42:20 -0500

    [login-cookie] Added styling to select dropdown

--- client/src/common/components/SingleSelectSearch/SingleSelectSearch.tsx
+++ client/src/common/components/SingleSelectSearch/SingleSelectSearch.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable react/no-array-index-key -- disabled */
+/* eslint-disable @typescript-eslint/no-explicit-any -- disabled */
+/* eslint-disable comma-spacing -- disabled */
+/* eslint-disable @typescript-eslint/comma-dangle -- disabled */
+
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
@@ -7,6 +12,9 @@ import styles from "./SingleSelectSearch.module.css";
 type SingleSelectSearchProperties<T> = {
     caret?: boolean;
     customContainerClassName?: string;
+    customContainerCaretClassName?: string;
+    customContainerDropdownClassName?: string;
+    customContainerDropdownItemClassName?: string;
     customContainerInputClassName?: string;
     customDisplayField?: string | ((_argument: T) => string);
     customSort?: (_item1: T, _item2: T) => number;
@@ -22,6 +30,9 @@ type SingleSelectSearchFunctionalSignature = <T>(
  * @param props - The properties of the single select search component
  * @param props.caret - Whether to display the caret in the select component or not
  * @param props.customContainerClassName - The custom class name to apply to the container, will override the default css applied
+ * @param props.customContainerCaretClassName - The custom class name to apply to the container's caret style, will override the default css applied
+ * @param props.customContainerDropdownClassName - The custom class name to apply to the container's dropdown style, will override the default css applied
+ * @param props.customContainerDropdownItemClassName - The custom class name to apply to the container's dropdown item's style, will override the default css applied
  * @param props.customContainerInputClassName - The custom class name to apply to the container's input, will override the default css applied
  * @param props.customDisplayField - Whether the items being passed in are custom, and the display is not as simple as just displaying the element
  * @param props.items - The items to display
@@ -30,27 +41,86 @@ type SingleSelectSearchFunctionalSignature = <T>(
 export const SingleSelectSearch: SingleSelectSearchFunctionalSignature = <T,>({
     caret,
     customContainerClassName,
+    customContainerCaretClassName,
+    customContainerDropdownClassName,
+    customContainerDropdownItemClassName,
     customContainerInputClassName,
     customDisplayField,
     customSort,
     items,
-}: SingleSelectSearchProperties<T>) => (
-    <div
-        className={`${styles.single_select_container} ${
-            customContainerClassName ?? ""
-        }`}
-    >
-        <input
-            className={`${styles.single_select_input} ${
-                customContainerInputClassName ?? ""
+}: SingleSelectSearchProperties<T>) => {
+    console.log("hello");
+    return (
+        <div
+            className={`${styles.single_select_container} ${
+                customContainerClassName ?? ""
             }`}
-            type="text"
-        />
-        {caret && (
-            <FontAwesomeIcon
-                className={`position-absolute ${styles.single_select_caret}`}
-                icon={faCaretDown}
+        >
+            <input
+                className={`${styles.single_select_input} ${
+                    customContainerInputClassName ?? ""
+                }`}
+                type="text"
             />
-        )}
-    </div>
-);
+            {caret && (
+                <FontAwesomeIcon
+                    className={`position-absolute ${
+                        styles.single_select_caret
+                    } ${customContainerCaretClassName ?? ""}`}
+                    icon={faCaretDown}
+                />
+            )}
+            <div
+                className={`position-absolute ${
+                    styles.single_select_dropdown
+                } ${customContainerDropdownClassName ?? ""}`}
+            >
+                {items.map((eachItem: T, _ind: number): JSX.Element => {
+                    if (typeof customDisplayField === "string") {
+                        return (
+                            <div
+                                className={`${
+                                    styles.single_select_dropdown_item
+                                } ${
+                                    customContainerDropdownItemClassName ?? ""
+                                }`}
+                                key={`eachItem-${_ind}-${eachItem}`}
+                            >
+                                {
+                                    (
+                                        eachItem as unknown as {
+                                            [key: string]: string;
+                                        }
+                                    )[customDisplayField]
+                                }
+                            </div>
+                        );
+                    } else if (typeof customDisplayField === "function") {
+                        return (
+                            <div
+                                className={`${
+                                    styles.single_select_dropdown_item
+                                } ${
+                                    customContainerDropdownItemClassName ?? ""
+                                }`}
+                                key={`eachItem-${_ind}-${eachItem}`}
+                            >
+                                {customDisplayField(eachItem)}
+                            </div>
+                        );
+                    }
+                    return (
+                        <div
+                            className={`${styles.single_select_dropdown_item} ${
+                                customContainerDropdownItemClassName ?? ""
+                            }`}
+                            key={`eachItem-${_ind}-${eachItem}`}
+                        >
+                            {eachItem as unknown as string}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
