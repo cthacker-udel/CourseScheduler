1dd114b5bc83c221a6423a560a4cf9d740afa9e3
cthacker-udel <cthacker@udel.edu>
2022-11-22 23:12:36 -0500

    [login-cookie] Added visibility functionality without any state

--- client/src/common/components/SingleSelectSearch/SingleSelectSearch.tsx
+++ client/src/common/components/SingleSelectSearch/SingleSelectSearch.tsx
@@ -9,6 +9,9 @@ import React from "react";
 
 import styles from "./SingleSelectSearch.module.css";
 
+/**
+ * The properties of the Single Select component
+ */
 type SingleSelectSearchProperties<T> = {
     caret?: boolean;
     customContainerClassName?: string;
@@ -16,16 +19,60 @@ type SingleSelectSearchProperties<T> = {
     customContainerDropdownClassName?: string;
     customContainerDropdownItemClassName?: string;
     customContainerInputClassName?: string;
+    customContainerInputOnClick?: (..._arguments: any) => any;
     customDisplayField?: string | ((_argument: T) => string);
     customSort?: (_item1: T, _item2: T) => number;
     items: T[];
 };
 
+/**
+ * The functional signature of the Single Select functional component
+ */
 type SingleSelectSearchFunctionalSignature = <T>(
     _properties: SingleSelectSearchProperties<T>,
 ) => JSX.Element;
 
 /**
+ * Handles the single select dropdown container input click, adding the class name `visible` to the dropdown select
+ */
+const handleSingleSelectContainerInputClick = (
+    event: MouseEvent,
+): void => {
+    const { target } = event;
+    if (target !== undefined) {
+        const htmlTarget = target as HTMLElement;
+        const singleSelectContainer = htmlTarget.closest(
+            "#single_select_container",
+        );
+        if (singleSelectContainer === null) {
+            // If container is not hit, then toggle off single select dropdown **if** it has the visibility class name present
+            const singleSelectDropdown = document.querySelector("#single_select_dropdown");
+            if (singleSelectDropdown === null) {
+                console.info("Unable to remove visibility from single select dropdown");
+            } else if (singleSelectDropdown.className.includes(styles.single_select_dropdown_visible)) {
+                const formattedClassName = `${singleSelectDropdown.className.replace(styles.single_select_dropdown_visible, "")} ${styles.single_select_dropdown_invisible}`;
+                singleSelectDropdown.className = formattedClassName;
+            }
+        } else {
+            // Hit container, if single select dropdown is not visible, toggle on, otherwise, do nothing
+            const singleSelectDropdown = document.querySelector(
+                "#single_select_dropdown",
+            );
+            if (
+                singleSelectDropdown !== null &&
+                !singleSelectDropdown.className.includes(
+                    styles.single_select_dropdown_visible,
+                )
+            ) {
+                // Is not visible, toggle on
+                singleSelectDropdown.className = `${singleSelectDropdown.className.replace(styles.single_select_dropdown_invisible, "")} ${styles.single_select_dropdown_visible}`;
+            }
+        }
+    }
+};
+
+/**
+ * The abstract component to handle when the user desires a component for having input select functionality, while also having a search functionality as well.
  *
  * @param props - The properties of the single select search component
  * @param props.caret - Whether to display the caret in the select component or not
@@ -34,9 +81,10 @@ type SingleSelectSearchFunctionalSignature = <T>(
  * @param props.customContainerDropdownClassName - The custom class name to apply to the container's dropdown style, will override the default css applied
  * @param props.customContainerDropdownItemClassName - The custom class name to apply to the container's dropdown item's style, will override the default css applied
  * @param props.customContainerInputClassName - The custom class name to apply to the container's input, will override the default css applied
+ * @param props.customContainerInputOnClick - The custom onClick handler for the single select input
  * @param props.customDisplayField - Whether the items being passed in are custom, and the display is not as simple as just displaying the element
  * @param props.items - The items to display
- * @returns
+ * @returns - The generic Single Select Search component
  */
 export const SingleSelectSearch: SingleSelectSearchFunctionalSignature = <T,>({
     caret,
@@ -45,21 +93,36 @@ export const SingleSelectSearch: SingleSelectSearchFunctionalSignature = <T,>({
     customContainerDropdownClassName,
     customContainerDropdownItemClassName,
     customContainerInputClassName,
+    customContainerInputOnClick,
     customDisplayField,
     customSort,
     items,
 }: SingleSelectSearchProperties<T>) => {
-    console.log("hello");
+    React.useEffect(() => {
+        // Append click listener for toggling on/off the visibility of the dropdown
+        document.addEventListener("click", handleSingleSelectContainerInputClick);
+
+        return () => {
+            document.removeEventListener("click", handleSingleSelectContainerInputClick);
+        };
+    }, []);
+
     return (
         <div
             className={`${styles.single_select_container} ${
                 customContainerClassName ?? ""
             }`}
+            id="single_select_container"
         >
             <input
                 className={`${styles.single_select_input} ${
                     customContainerInputClassName ?? ""
                 }`}
+                id="single_select_input"
+                onClick={
+                    customContainerInputOnClick ??
+                    handleSingleSelectContainerInputClick
+                }
                 type="text"
             />
             {caret && (
@@ -73,7 +136,8 @@ export const SingleSelectSearch: SingleSelectSearchFunctionalSignature = <T,>({
             <div
                 className={`position-absolute ${
                     styles.single_select_dropdown
-                } ${customContainerDropdownClassName ?? ""}`}
+                } ${customContainerDropdownClassName ?? ""} ${styles.single_select_dropdown_invisible}`}
+                id="single_select_dropdown"
             >
                 {items.map((eachItem: T, _ind: number): JSX.Element => {
                     if (typeof customDisplayField === "string") {
